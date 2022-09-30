3c670245966337e1a27e9dcac47888cc8148b453
davidlandy05 <113514395+davidlandy05@users.noreply.github.com>
2022-09-30 12:35:13 +0100

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,11 @@
+//  Qustion 1 =*,-,/,+ =operater 
+// Question 1=-88.8,5,"hello"= values
+
+//question 2 = "spam"
+
+// Question 3 = string,boolean and number
+
+// Question 4 = 21
+
+// Question 5 spamspamspam
+// Question 5 = null //
