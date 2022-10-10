3d1ca9722bf5a76deaa6327032b0e76172c4783e
davidlandy05 <113514395+davidlandy05@users.noreply.github.com>
2022-10-10 11:54:18 +0100

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,17 @@
+//While loop execute a statement
+//
+
+let count=0;
+while(count<10){
+    console.log(count);
+    count++; //count = count+1
+}
+
+// Do While Loop
+// Always Runs The Code
+//At Least Once
+do[
+    console.log(count),
+    count++
+
+];while(count<20);
