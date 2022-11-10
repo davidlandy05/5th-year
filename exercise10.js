625d35dcc519b84b4fd401e421ee893fbeb1b85b
davidlandy05 <113514395+davidlandy05@users.noreply.github.com>
2022-11-10 11:55:08 +0000

    Add files via upload

--- exercise10.js
+++ exercise10.js
@@ -33,3 +33,14 @@ for(let k = 0; k<c; k++){
     }    }
     document.write("<br />");
 }document.write("<pre/>")
+
+
+let d = Number(prompt("enter the number of rows"));
+
+let e = d
+
+for(let z = 0;z<d;r++){
+for(let x = 0;x<e;k++){
+    
+}
+}
